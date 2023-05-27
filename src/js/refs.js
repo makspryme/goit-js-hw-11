8e771360d83e0437223ecfc0c84a0889caee1530
makspryme <makspryme17@gmail.com>
2023-05-27 17:48:59 +0300

    f

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,1 @@
-export default refs = {
-  gallary: document.querySelector('.gallery'),
-  inputSearch: document.querySelector('input[name="searchQuery"]'),
-  form: document.querySelector('#search-form'),
-  btnMore: document.querySelector('.load-more'),
-};
+
